f7bc998ab7852c22509804209c2a1ac7ad65060d
Parker Tichko <parker.tichko@uconn.edu>
2020-09-22 14:26:54 -0400

    Blogpost about d-prime stats in R

---
+++ r/dprime_post.R
@@ -0,0 +1,68 @@
+
+#' Calculate D-Prime from D-Prime Summary Statistics
+#'
+#' Note: Must run dprime_pre.r first.
+#' Calculates dprime from summary stats: hits, misses, false alarms, and correct rejections.
+#' These columns must already be in the data frame that is passed in as an argument.
+#'
+#'
+#' @param df is the data farm with columns corresonding to d-prime summary statistics
+#' @param ... names of cols corresponding to IVs to group by (supports n > 1 cols). IVs must be factors.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' # make data frame
+#' df<- data.frame(participant = c(rep("Participant 1",24), rep("Participant 2",24,),
+#' rep("Participant 3",24), rep("Participant 4",24)),
+#'                stim1 = rep(c(rep(c("Apple", "Orange", "Orange", "Apple"), 6),
+#'                              rep(c("Orange", "Apple", "Apple", "Orange"), 6)),2),
+#'                stim2 = rep(c(rep(c("Apple", "Orange", "Apple", "Orange"), 6),
+#'                              rep(c("Orange", "Apple", "Apple", "Orange"), 6)),2),
+#'                correct = c(rep(c(1,1,0,0),1), rep(c(1,0,1,0),1),
+#'                            rep(c(0,0,1,1),2), rep(c(0,1,0,1),2),
+#'                            rep(c(0,1,0,1),2), rep(c(0,0,1,1),2),
+#'                            rep(c(1,0,1,0),1), c(rep(c(1,1,0,0),1))))
+#'
+#'
+#' # reorder by participant
+#' df <- df[order(df[,"participant"]),]
+#'
+#' # calculate dprime summary stats
+#' df.dprime <- dprime_pre("stim1", "stim2", "correct", data = df)
+#'
+#' # calculate dprime
+#' dprime_post(df.dprime, participant)
+dprime_post <- function(df, ...){
+
+  require(dplyr)
+  require(tibble)
+
+  # drops NAs
+  na.rm = TRUE
+
+  # quote input args to use with dplyr functions
+  group_vars <- dplyr::quos(...)
+
+  # convert to tibble, if not already tibble
+  if(tibble::is_tibble(df) == FALSE){
+    df <- tibble::as_tibble(df)}
+
+  # group by df variables
+  df <- dplyr::group_by(df,!!! group_vars)
+
+  dplyr::summarise(df,
+                   Hits = sum(dprime.Hit, na.rm = na.rm),
+                   Misses = sum(dprime.Miss, na.rm = na.rm),
+                   FalseAlarms = sum(dprime.FalseAlarm, na.rm = na.rm),
+                   CorrectRejs = sum(dprime.CorrectRej, na.rm = na.rm),
+                   Total = Hits + Misses + FalseAlarms + CorrectRejs,
+                   HitRate = if(Hits/Total == 1 | Hits/Total ==  0){Hits/Total + 0.00001} else{Hits/Total}, # qunorm(1) & qnorm(0) are Inf. Increase HitRate and Missrate by 0.00001.
+                   MissRate = if(Misses/Total == 1 |Misses/Total == 0){Misses/Total + 0.00001} else{Misses/Total}, # qunorm(1) & qnorm(0) are Inf. Increase HitRate and Missrate by 0.00001.
+                   FalseAlarmRate = FalseAlarms/Total,
+                   CorrectRejRate = CorrectRejs/Total,
+                   Dprime = qnorm(HitRate) - qnorm(MissRate))
+
+}
---
+++ r/dprime_pre.R
@@ -0,0 +1,62 @@
+#' Calculate D-Prime Summary Statistics
+#'
+#' Calculates d-prime summary stats and appends them as columns to a data frame.
+#' Adds columns for hits, misses, false alarms, and correct rejections.
+#'
+#' @param stim1_col column that has the filename of target stimulus
+#' @param stim2_col column that has the filename of probe stimulus
+#' @param correct_col column that has correct/incorrect responses, where 1 is correct and 0 is incorrect
+#' @param data is a data frame
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' #Make DF
+#' df<- data.frame(participant = c(rep("Participant 1",24), rep("Participant 2",24,),
+#' rep("Participant 3",24), rep("Participant 4",24)),
+#'                stim1 = rep(c(rep(c("Apple", "Orange", "Orange", "Apple"), 6),
+#'                              rep(c("Orange", "Apple", "Apple", "Orange"), 6)),2),
+#'                stim2 = rep(c(rep(c("Apple", "Orange", "Apple", "Orange"), 6),
+#'                              rep(c("Orange", "Apple", "Apple", "Orange"), 6)),2),
+#'                correct = c(rep(c(1,1,0,0),1), rep(c(1,0,1,0),1),
+#'                            rep(c(0,0,1,1),2), rep(c(0,1,0,1),2),
+#'                            rep(c(0,1,0,1),2), rep(c(0,0,1,1),2),
+#'                            rep(c(1,0,1,0),1), c(rep(c(1,1,0,0),1))))
+#'
+#' # Calculate correct rejections, hits, misses, and false alarms
+#' dprime_pre("stim1", "stim2", "correct", data = df)
+#'
+dprime_pre <- function(stim1_col, stim2_col, correct_col, data){
+
+  # Compute sub-categories for d-prime stats
+  for(i in 1:length(data[[stim1_col]])){
+
+    if(data[i,stim1_col] != data[i, stim2_col] & data[i, correct_col] == 1){
+
+      # HIT: different stim, correct response
+    data[i,"dprime.Hit"] <- 1
+
+    } else if(data[i,stim1_col] != data[i,stim2_col] & data[i,correct_col] == 0){
+
+    # MISS: different stim, incorrect response
+    data[i,"dprime.Miss"] <- 1
+
+    } else if(data[i,stim1_col] == data[i,stim2_col] & data[i,correct_col] == 0){
+
+      # FALSE ALARM: same stim, incorrect resonse
+      data[i, "dprime.FalseAlarm"] <- 1
+
+    } else if(data[i,stim1_col] == data[i,stim2_col] & data[i,correct_col] == 1){
+
+      # CORRECT REJECTION: same stim, correct response
+      data[i, "dprime.CorrectRej"] <- 1
+
+
+    }
+
+  }
+
+  return(data)
+}
